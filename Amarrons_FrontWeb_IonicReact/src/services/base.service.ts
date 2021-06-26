a596421776bfd50078f708da6af8be0273151f11
Florian Gérard <kingdomflo16@gmail.com>
2021-06-26 17:09:38 +0200

    remove useless await async

--- Amarrons_FrontWeb_IonicReact/src/services/base.service.ts
+++ Amarrons_FrontWeb_IonicReact/src/services/base.service.ts
@@ -3,11 +3,11 @@ import Axios from "axios"
 // TODO gestion of the api_key
 export class BaseService {
 
-  static async get(url: string) {
-    return await Axios.get(process.env.REACT_APP_API_URL + url, { headers: { user_token: localStorage.getItem('user_token') } });
+  static get(url: string) {
+    return Axios.get(process.env.REACT_APP_API_URL + url, { headers: { user_token: localStorage.getItem('user_token') } });
   }
 
-  static async post(url: string, body: any) {
+  static post(url: string, body: any) {
     return Axios.post(process.env.REACT_APP_API_URL + url, body, { headers: { user_token: localStorage.getItem('user_token') } });
   }
 
